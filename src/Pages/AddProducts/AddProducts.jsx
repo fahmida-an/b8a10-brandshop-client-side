ebfe4bd54ddb4e94edaca968fd4204d44af94ec2
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-19 02:36:57 +0600

    Added profile on the navbar

---
+++ src/Pages/AddProducts/AddProducts.jsx
@@ -0,0 +1,9 @@
+const AddProducts = () => {
+    return (
+        <div>
+            <h2>This is private add products</h2>
+        </div>
+    );
+};
+
+export default AddProducts;
--- src/Sharedpages/Header/Navbar.jsx
+++ src/Sharedpages/Header/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   const navLinks = (
     <>
       <li>
@@ -35,11 +38,7 @@ const Navbar = () => {
         <NavLink
           to="/blog"
           className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? " hover:text-slate-100 mr-2"
-              : ""
+            isPending ? "pending" : isActive ? " hover:text-slate-100 mr-2" : ""
           }
         >
           MyCart
@@ -87,37 +86,68 @@ const Navbar = () => {
               </ul>
             </div>
             {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
-            
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1 text-white">{navLinks}</ul>
           </div>
 
           <div className="navbar-end">
-          <NavLink
-              to="/login"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? "text-red-500 rounded-lg p-2 "
-                  : "text-gray-900 mr-4  btn btn-sm"
-              }
-            >
-              Login
-            </NavLink>
-            <NavLink
-              to="/register"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? " text-red-500 rounded-lg p-2"
-                  : " text-gray-900 btn btn-sm"
-              }
-            >
-              SignUp
-            </NavLink>
+            {user?.email ? (
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={user.photoURL} alt={user} />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box lg:w-72 md:72 w-64"
+                >
+                  <li>
+                    <button className="btn btn-sm btn-ghost text-pinkdark1 hover:bg-pinkdark1 text-sm mb-1 w-full">
+                      {user.displayName}
+                    </button>
+                  </li>
+                  <li>
+                    <button className="btn btn-sm btn-ghost text-pinkdark1 hover:bg-pinkdark1 text-sm mb-1 w-full">
+                      {user.email}
+                    </button>
+                  </li>
+                  <li>
+                    <button className="btn btn-sm  btn-ghost text-pinkdark1 hover:bg-pinkdark1 text-sm mb-1 w-full">
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div>
+                <NavLink
+                  to="/login"
+                  className={({ isActive, isPending }) =>
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "text-red-500 rounded-lg p-2 "
+                      : "text-gray-900 mr-4  btn btn-sm"
+                  }
+                >
+                  Login
+                </NavLink>
+                <NavLink
+                  to="/register"
+                  className={({ isActive, isPending }) =>
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? " text-red-500 rounded-lg p-2"
+                      : " text-gray-900 btn btn-sm"
+                  }
+                >
+                  SignUp
+                </NavLink>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -126,4 +156,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
