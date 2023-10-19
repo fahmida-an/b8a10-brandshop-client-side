7f559a738fc58459df5b7aefde11baec3c65004e
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-19 14:22:56 +0600

    Product added to the database

--- src/Pages/AddProducts/AddProducts.jsx
+++ src/Pages/AddProducts/AddProducts.jsx
@@ -1,14 +1,55 @@
 import Logo from "../../Sharedpages/Header/Logo";
-
+import swal from 'sweetalert';
 const AddProducts = () => {
-    
+  const handleAddProduct = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const image = form.image.value;
+    const name = form.name.value;
+    const brandname = form.brandname.value;
+    const type = form.select.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const details = form.details.value;
+    const newProducts = {
+      image,
+      name,
+      brandname,
+      type,
+      price,
+      rating,
+      details,
+    };
+    console.log(newProducts);
+
+    //server connection 
+    fetch('http://localhost:4000/products', {
+        method: "POST",
+        headers: {
+            "content-type" : "application/json"
+        },
+        body: JSON.stringify(newProducts)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.insertedId > 0){
+            swal("Successfully", "Product Added!", "success");
+
+        }
+    })
+  };
+
   return (
     <div>
       <h2>This is private add products</h2>
       <Logo></Logo>
       <div className="max-w-6xl mx-auto">
         <h2 className="text-center py-8 font-bold text-5xl">Add a Product</h2>
-        <form className="bg-base-300 max-w-xl mx-auto p-8">
+        <form
+          onSubmit={handleAddProduct}
+          className="bg-base-300 max-w-xl mx-auto p-8"
+        >
           <div className="form-control max-w-xl">
             <label className="label">
               <span className="label-text">Image</span>
@@ -40,14 +81,21 @@ const AddProducts = () => {
             <label className="label">
               <span className="label-text">Brand Name</span>
             </label>
+           
             <label className="input-group">
-              <input
-                type="text"
-                name="brandname"
-                placeholder="Brand Name"
-                className="input input-bordered w-full"
-              />
+              <select name="brandname" className="select select-bordered w-full ">
+                <option disabled selected>
+                  Brand Name
+                </option>
+                <option value="Dior">Dior</option>
+                <option value="Addidas">Addidas</option>
+                <option value="Nike">Nike</option>
+                <option value="Aarong">Aarong</option>
+                <option value="Zara">Zara</option>
+                <option value="Gucci">Gucci</option>
+              </select>
             </label>
+       
           </div>
 
           <div className="form-control">
@@ -59,12 +107,12 @@ const AddProducts = () => {
                 <option disabled selected>
                   type
                 </option>
-                <option>Watch</option>
-                <option>Shoes</option>
-                <option>Jewellery</option>
-                <option>Foundation</option>
-                <option>Lipsticks</option>
-                <option>Eye Shadow</option>
+                <option value="Watch">Watch</option>
+                <option value="Shoes">Shoes</option>
+                <option value="Jewellery">Jewellery</option>
+                <option value="Foundation">Foundation</option>
+                <option value="Lipsticks">Lipsticks</option>
+                <option value="Eye Shadow">Eye Shadow</option>
               </select>
             </label>
           </div>
